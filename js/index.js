bc77ab4eb8863e1466bdb1d881f9ae1fa769b4dc
Bonnie Turnbeaugh <turnbeaugh@comcast.net>
2020-02-11 18:39:20 -0800

    Added selectors and filled content

--- js/index.js
+++ js/index.js
@@ -38,5 +38,56 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
+
+// Images - Logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// Images - CTA
+let heroImg = document.getElementById('cta-img');
+heroImg.setAttribute('src', siteContent['cta']['img-src']);
+
+// Images - Middle
+
+let middleImg = document.getElementById('middle-img');
+middleImg.setAttribute('src', siteContent['main-content']['middle-img-src']);
+
+// Nav
+const topNav = document.querySelectorAll('nav a');
+topNav[0].textContent = 'Services';
+topNav[1].textContent = 'Product';
+topNav[2].textContent = 'Vision';
+topNav[3].textContent = 'Features';
+topNav[4].textContent = 'About';
+topNav[5].textContent = 'Contact';
+
+// CTA
+let mainTag = document.querySelector('h1');
+mainTag.textContent = 'DOM IS AWESOME';
+
+let mainBtn = document.querySelector('button');
+mainBtn.textContent = 'Get Started';
+
+// Main-Content
+let artHeaders = document.querySelectorAll('h4');
+artHeaders[0].textContent = 'Features';
+artHeaders[1].textContent = 'About';
+artHeaders[2].textContent = 'Services';
+artHeaders[3].textContent = 'Product';
+artHeaders[4].textContent = 'Vision';
+artHeaders[5].textContent = 'Contact';
+
+let artPara = document.querySelectorAll('section p');
+artPara[0].textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+artPara[1].textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+artPara[2].textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+artPara[3].textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+artPara[4].textContent = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+artPara[5].textContent = '123 Way 456 Street Somewhere, USA';
+artPara[6].textContent = '1 (888) 888-8888';
+artPara[7].textContent = 'Sales@greatidea.io';
+
+// Footer
+
+let copyright = document.querySelector('footer p');
+copyright.textContent = 'Copyright Great Idea! 2018';
